3107416accb961ba36bb4c75f371ba0e03fd1dff
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2021-05-13 17:28:50 +0200

    docs

---
+++ skills/index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>your_domain website</title>
+  </head>
+  <body>
+    <h1>Hello BalearSkills</h1>
+
+    
+    <?php echo "<p>La teva web dels <strong>BalearSkills</strong>.</p>"; ?>
+    <?php phpinfo(); ?>
+  </body>
+</html>
